2aacfd22a03513e02f77fbdfeec481533bc20620
Afzal <arfimm@gmail.com>
2026-03-06 22:54:59 +0000

    update

--- backend.tf
+++ backend.tf
@@ -1,12 +1,12 @@
 
 terraform {
 
-  required_version = ">= 1.5"
+  required_version = ">= 1.14.0"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.0"
+      version = "~> 7.0"
     }
   }
 
